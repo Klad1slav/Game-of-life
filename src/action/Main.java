ed00ae4b6b7310597fa55bf8b14540ce85dfea09
Vlad <vladmajstrenko13@gmail.com>
2024-03-05 12:56:57 +0100

    Game of Life-Das Spiel

---
+++ src/action/Main.java
@@ -0,0 +1,21 @@
+package action;
+
+import game.GM;
+import game.GameClock;
+import gui.Gui;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Gui g = new Gui();
+        GM gm = new GM();
+        GameClock gc = new GameClock();
+
+        gm.setup();
+        g.create();
+        gc.start();
+
+
+
+    }
+}
